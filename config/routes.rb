1c74b31fa2bb1b69335e6757ae049be9b766b855
Mai Đức Thắng <maiducthang97@gmail.com>
2018-03-15 15:42:47 +0700

    fix image edit

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   get '/index/user/:id/posts', to: 'posts#index_user_posts', as: 'index_user_posts' #liệt kê toàn bộ các bài viết của user
   
   # add a collection search - Q
-  resources :posts, except: [:new, :edit, :update, :destroy, :create] do
+  resources :posts, except: [:destroy] do
     collection do
       get 'search'
       get 'result'
