d44c2e9568ccffc533486ad7a926ffc01dc2fa0f
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-03-26 13:41:04 +0700

    nothing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'stormy-reaches-10035.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    user_name:      'hoanhtungtest@gmail.com',
+    password:       'hoanhtung.xy',
+    domain:         'stormy-reaches-10035.herokuapp.com',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
