3fa496ae49fbf569c6f3bc273bd969eae3f780e3
Mai Đức Thắng <maiducthang97@gmail.com>
2018-03-26 16:19:22 +0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,94 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-cities = City.create!([
-    {name: "Hồ Chí Minh"},
-    {name: "Hà Nội"},
-    {name: "Đà Nẵng"}
-])
-
-districts_1 = cities[0].districts.create!([
-    {name: "Quận 1"},
-    {name: "Quận 2"},
-    {name: "Quận 3"},
-    {name: "Quận 4"},
-    {name: "Quận 5"},
-    {name: "Quận 6"},
-    {name: "Quận 7"},
-    {name: "Quận 8"},
-    {name: "Quận 9"},
-    {name: "Quận 10"},
-    {name: "Quận 11"},
-    {name: "Quận 12"},
-    {name: "Quận Thủ Đức"},
-    {name: "Quận Gò Vấp"}
-])
-
-districts_2= cities[1].districts.create!([
-    {name: "Quận Ba Đình"},
-    {name: "Quận Bắc Từ Liêm"},
-    {name: "Quận Cầu Giấy"},
-    {name: "Quận Đống Đa"},
-    {name: "Quận Hà Đông"},
-    {name: "Quận Ba Vì"},
-    {name: "Quận Phú Xuyên"},
-    {name: "Quận Phú Thọ"}
-])
-
-districts_3 = cities[2].districts.create!([
-    {name: "Quận Côn Đảo"},
-    {name: "Quận Long Điền"},
-    {name: "Quận Châu Đức"},
-    {name: "Quận Đất Đỏ"},
-    {name: "Quận Xuyên Mộc"},
-    {name: "Quận Vũng Tàu"},
-    {name: "Quận Tân Thành"},
-    {name: "Quận Bà Rịa"}
-])
-
-
-districts_1.each do |d|
-    wards = d.wards.create!([
-        {name: "Phường Bến Nghé"},
-        {name: "Phường Bến Thành"},
-        {name: "Phường Đa Kao"},
-        {name: "Phường Trương Định"},
-        {name: "Phường Phạm Ngũ Lão"}
-    ])
-    wards.each do |w|
-        5.times do |i|
-            w.streets.create!({name: "Đường #{i}"})
-        end
-    end
-end
-
-districts_2.each do |d|
-    wards = d.wards.create!([
-        {name: "Phường Kim Mã"},
-        {name: "Phường Điện Biên"},
-        {name: "Phường Thành Công"},
-        {name: "Phường Trúc Bạch"},
-        {name: "Phường Liễu Giai"}
-    ])
-    wards.each do |w|
-        5.times do |i|
-            w.streets.create!({name: "Đường #{i}"})
-        end
-    end
-end
-
-districts_3.each do |d|
-    wards = d.wards.create!([
-        {name: "Phường Phong Tâm"},
-        {name: "Phường Long Toàn"},
-        {name: "Phường Phước Trung"},
-        {name: "Xã Tân Hưng"},
-        {name: "Xã Hòa Long"}
-    ])
-    wards.each do |w|
-        5.times do |i|
-            w.streets.create!({name: "Đường #{i}"})
+dataset = JSON.parse(File.read('db/data.json'))
+dataset.each do |ckey,cvalue|
+    city = City.create!(name: cvalue[:name])
+    cvalue['quan-huyen'].each do |dk, dv|
+        district = city.districts.create!(name: dv[:name])
+        dv['xa-phuong'].each do |wk, wv|
+            ward = district.wards.create!(name: wv[:name])
+            2.times do |i|
+                ward.streets.create!(name: "Đường #{i}")
+            end
         end
     end
 end
