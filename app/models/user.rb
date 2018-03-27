9e03e77875e0a825507bc46e48f44f7815b37f4d
minhquan2904 <minhquan2904@outlook.com>
2018-03-27 11:56:46 +0700

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      if auth.info.email
+      if !auth.info.email.blank?
         user.email = auth.info.email
       else
         user.email = "auth.uid"
