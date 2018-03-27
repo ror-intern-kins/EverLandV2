576035e31e3ae3836733830ca7b37f096f4cb4cf
Yen Minh <minhhy2897@gmail.com>
2018-03-27 11:50:24 +0700

    fix

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -16,7 +16,7 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def google_oauth2
-    @user = User.create_from_google_data(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,16 +27,4 @@ class User < ApplicationRecord
       end
     end
   end
- 
-  #GG
-  def self.find_or_create_from_auth_hash(auth)
-		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-			user.provider = auth.provider
-      user.uid = auth.uid
-      user.name = auth.info.name 
-      user.username = auth.info.email
-			user.email = auth.info.email
-			user.save!
-		end
-	end
 end
