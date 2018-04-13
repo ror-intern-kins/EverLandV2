899710ddc62dfdf65654f85d1360f5a60ffb5bcd
Hoanh Tung <hoanhtung.xy@gmail.com>
2018-04-13 14:40:02 +0700

    cmt model again

--- app/models/post.rb
+++ app/models/post.rb
@@ -48,29 +48,29 @@ class Post < ApplicationRecord
     # validate :district_id_exists
     
     #Can not be blank
-    validates :title, :description, :category_id, :city_id,
-        :district_id, :address_number, :contact_mobile, presence: true
-    #Length
-    validates :title, length: {minimum:30, maximum:99}
-    validates :description, length: {minimum:30, maximum:3000}
-    validates :address_number,length: {maximum:200}
-    validates :furniture, length: {maximum:200, allow_blank: true}  
-    validates :contact_address, length: {maximum:200, allow_blank: true}    
-        #Format
-    validates :contact_mobile, length: {minimum:10, maximum:11}, format: {with: /\d{10,11}/}
-    validates :contact_phone, length: {minimum:10, maximum:11},  format: {with: /\d{10,11}\z/}, allow_blank:true
-    validates :contact_mail, allow_blank:true, length: {maximum: 100}, 
-        format: { with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i}
-    validates :contact_name, allow_blank:true, length: {maximum:200}, format: {with: /\A[a-zA-Z]+\z?/}
-    validates :project, allow_blank:true, length: {maximum:200}, format: {with: /\A[a-zA-Z]+\z?/}
-        #Num
-    validates :price, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999}
-    validates :area, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999 }
-    validates :front, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999}
-    validates :entrance, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999}
-    validates :floor, allow_blank:true, numericality: {only_integer: true, greater_than:0, less_than_or_equal_to: 999}
-    validates :bedroom, allow_blank:true, numericality: {only_integer: true, greater_than:0, less_than_or_equal_to: 999}
-    validates :toilet,allow_blank:true, numericality: {only_integer: true, greater_than:0, less_than_or_equal_to: 999}
+    # validates :title, :description, :category_id, :city_id,
+    #     :district_id, :address_number, :contact_mobile, presence: true
+    # #Length
+    # validates :title, length: {minimum:30, maximum:99}
+    # validates :description, length: {minimum:30, maximum:3000}
+    # validates :address_number,length: {maximum:200}
+    # validates :furniture, length: {maximum:200, allow_blank: true}  
+    # validates :contact_address, length: {maximum:200, allow_blank: true}    
+    #     #Format
+    # validates :contact_mobile, length: {minimum:10, maximum:11}, format: {with: /\d{10,11}/}
+    # validates :contact_phone, length: {minimum:10, maximum:11},  format: {with: /\d{10,11}\z/}, allow_blank:true
+    # validates :contact_mail, allow_blank:true, length: {maximum: 100}, 
+    #     format: { with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i}
+    # validates :contact_name, allow_blank:true, length: {maximum:200}, format: {with: /\A[a-zA-Z]+\z?/}
+    # validates :project, allow_blank:true, length: {maximum:200}, format: {with: /\A[a-zA-Z]+\z?/}
+    #     #Num
+    # validates :price, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999}
+    # validates :area, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999 }
+    # validates :front, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999}
+    # validates :entrance, allow_blank:true, numericality: {only_float: true, greater_than: 0, less_than_or_equal_to: 999999}
+    # validates :floor, allow_blank:true, numericality: {only_integer: true, greater_than:0, less_than_or_equal_to: 999}
+    # validates :bedroom, allow_blank:true, numericality: {only_integer: true, greater_than:0, less_than_or_equal_to: 999}
+    # validates :toilet,allow_blank:true, numericality: {only_integer: true, greater_than:0, less_than_or_equal_to: 999}
     
     protected
     def category_id_exists
